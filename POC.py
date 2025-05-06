6faac6a536542ef3761219e351a923a8d55d03cd
reishi <x1dragonn@gmail.com>
2025-05-06 23:54:44 +0000

    ~0.2

--- POC.py
+++ POC.py
@@ -2,35 +2,19 @@ import click
 import os 
 import sys
 import subprocess
-
-
-
-@click.command()
-@click.argument('domain')
-@click.option('--no-assetfinder',"-ns", help='This option disables assetfinder', is_flag=True)
-@click.option("--no-subfinder", "-nsf", help='This option disables subfinder', is_flag=True)
-
-def main(domain, no_assetfinder, no_subfinder):
-    click.echo(f"Starting enumeration for: {domain}")
-
-
-    if not no_assetfinder:
-        click.echo("Running assetfinder...")
-
-    if not no_subfinder:
-        click.echo("Running subfinder...")
-
-    
-    
-      
+from typing import List
 
 
 
 
+def run_command(cmd: List[str], input_data: str = None) -> str:
+    try:
+        results = subprocess.run(cmd, input=input_data, capture_output = True, text=True, check=True)
+        return results.stdout().strip()
+    except subprocess.CalledProcessError as e:
+        click.echo(f"Error running: {''.join(cmd)}:", err=True)
+        
 
-
-if __name__ == "__main__":
-    main()
 
 
 
